98de757b21a1661f3f6339413d406d881987d3c9
masonmcbride <mcbridedmason@gmail.com>
2024-08-08 11:22:20 -0700

    refactor

--- cong.py
+++ cong.py
@@ -1,15 +1,11 @@
 import numpy as np
-num_of_players = 5
+num_of_players = 2
 
-def divisors_gen(k):
-    d = 1
-    while d <= k:
-        if k % d == 0:
-            yield d
-        d += 1
+def divisors_gen(k: int):
+    return (d for d in range(1,k+1) if k % d == 0)
 
 def u(ki, kjs):
-    return sum([sum([1 if kj % d != 0 else 0 for d in divisors_gen(ki)]) for kj in kjs])
+    return sum(sum(kj % d != 0 for d in divisors_gen(ki)) for kj in kjs)
 
 def phi(strategy):
     uu = 0
@@ -18,32 +14,32 @@ def phi(strategy):
         uu += u(ki, kjs)
     return uu
 
-def split(strategy, i=0):
-    ki = strategy[i]
-    kjs = strategy[:i] + strategy[i+1:]
-    return ki, kjs
+def best_response(kjs):
+    br = np.argmax([u(i, kjs) for i in range(1000)])
+    return br
 
-def step(ki, kjs):
-    new_ki = np.argmax([u(i, kjs) for i in range(1000)])
-    changed = new_ki != ki
-    return new_ki, changed
+def solve(l):
+    while l != (new_l:=[best_response(l[:i] + l[i+1:]) for i in range(len(l))]):
+        print(f"{new_l=}")
 
-def solve(l=list(np.random.choice(1000, num_of_players))):
-    
-    count = 0
-    any_changed = True
-    while any_changed:
-        any_changed = False
-        for i in range(len(l)):
-            ki, kjs = split(l, i)
-            new_ki, changed = step(ki, kjs)
-            if changed: 
-                any_changed = True
-                l[i] = new_ki
-        count += 1
+        l = new_l 
     return l
 
-nash = solve()
-print(f"a nash equilibrium: {nash}")
-total_utility = phi(nash)
-print(f"total utility {total_utility}")
+from itertools import product
+from random import choice
+from collections import defaultdict
+all_combos = list(product(range(1000),repeat=num_of_players))
+results = defaultdict(int)
+print("starting")
+for i,combo in enumerate(all_combos):
+    if i % 1000 == 0:
+        print(f"run {i}")
+    br = [best_response(combo[:i] + combo[i+1:]) for i in range(len(combo))]
+    results[str(br)] += 1
+print(results)
+#initial_state = list(np.random.choice(1000, num_of_players))
+#print(f"{initial_state=}")
+#nash = solve(initial_state)
+#print(f"a nash equilibrium: {nash}")
+#total_utility = phi(nash)
+#print(f"total utility {total_utility}")
